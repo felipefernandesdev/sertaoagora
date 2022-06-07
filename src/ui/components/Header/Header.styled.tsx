5a682fb72c0f62c3575d90bc0bc03d1f2d80d6c0
Felipe Fernandes Dev <fesousalocutor@gmail.com>
2022-06-07 10:32:48 -0300

    edit updated

--- src/ui/components/Header/Header.styled.tsx
+++ src/ui/components/Header/Header.styled.tsx
@@ -1,14 +1,18 @@
 import { styled } from "@mui/material";
 
-export const HeaderStyled = styled("header")`
+const HeaderStyled = styled("header")`
   display: flex;
   justify-content: center;
   border-bottom: 1px solid #f0f0f0;
   padding: ${({ theme }) => theme.spacing(4)};
   background-color:#f0eeee;
 `
-export const Logo = styled('img')`
+const Logo = styled('img')`
   max-width: 100%;
   width: 320px;
   `
 
+export {
+  HeaderStyled,
+  Logo
+}
