4f8fa25e0f04945d12b57e2efbd5b13a04df0eed
Felipe Fernandes Dev <fesousalocutor@gmail.com>
2022-06-07 10:24:46 -0300

    create and update to yarn

--- src/ui/components/Header/Header.styled.tsx
+++ src/ui/components/Header/Header.styled.tsx
@@ -5,8 +5,10 @@ export const HeaderStyled = styled("header")`
   justify-content: center;
   border-bottom: 1px solid #f0f0f0;
   padding: ${({ theme }) => theme.spacing(4)};
+  background-color:#f0eeee;
 `
 export const Logo = styled('img')`
   max-width: 100%;
+  width: 320px;
   `
 
